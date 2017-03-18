af0972ca3be630f422aed1d5cc1d445c3a98fb03
shundroid <shundroidk@gmail.com>
2017-03-18 15:18:13 +0900

    fixed test

---
+++ test/actions.spec.js
@@ -0,0 +1,18 @@
+import { assert } from "chai";
+import { generateSimpleActions } from "../src/store";
+import sinon from "sinon";
+
+describe("actions", () => {
+  describe("generateSimpleActions", () => {
+    it("should generate actions", () => {
+      const mutationName = "mutation-1";
+      const mutations = [mutationName];
+      const payload = "payload";
+      const actions = generateSimpleActions(mutations);
+      assert(actions[mutationName]);
+      const spy = sinon.spy();
+      actions[mutationName]({ commit: spy }, payload);
+      assert(spy.withArgs(mutationName, payload).calledOnce);
+    });
+  });
+});
