728da3dc3537c4df148596d56dd1328e9498ac9f
shundroid <shundroidk@gmail.com>
2017-03-18 22:48:59 +0900

    added header buttons

--- src/components/nHeader.vue
+++ src/components/nHeader.vue
@@ -1,7 +1,14 @@
-<template>
-  <md-toolbar class="md-whiteframe-2dp n-header">
-    <h1 class="md-title">EV3 Notes Maker</h1>
-  </md-toolbar>
+<template lang="pug">
+  md-toolbar.md-whiteframe-2dp.n-header
+    h1.md-title EV3 Notes Maker
+    md-button.md-icon-button
+      md-icon play_arrow
+    md-button.md-icon-button
+      md-icon save
+    md-button.md-icon-button
+      md-icon open_in_browser
+    md-button.md-icon-button
+      md-icon folder_open
 </template>
 
 <script>
@@ -9,4 +16,7 @@ export default {};
 </script>
 
 <style scoped>
+.md-title {
+  flex: 1;
+}
 </style>
